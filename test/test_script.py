7e37cf69fe5b81cca5a96f56d7b1468d181b7758
gcazcarroceste <gcazcarro@ceste.com>
2026-01-07 09:45:23 +0100

    Add greeting function and main script execution

---
+++ test/test_script.py
@@ -0,0 +1,7 @@
+def obtener_saludo():
+    """Retorna un saludo estandar"""
+    return "Hello world"
+
+if __name__ == "__main__":
+    # Esto solo se ejecuta si lanzas el script directamente, no al importarlo
+    print(obtener_saludo())
