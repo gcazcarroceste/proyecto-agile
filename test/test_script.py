b135b54761ff82538d9ee5b0f01aacf348b79613
gcazcarroceste <gcazcarro@ceste.com>
2026-01-07 13:39:25 +0100

    Update test_script.py

--- test/test_script.py
+++ test/test_script.py
@@ -4,9 +4,9 @@ import operaciones
 class TestOperaciones(unittest.TestCase):
 
     def test_suma(self):
-        self.assertEqual(operaciones.suma(3, 2), 5)
-        self.assertEqual(operaciones.suma(-1, 1), 0)
-        self.assertEqual(operaciones.suma(-1, -1), -2)
+        self.assertEqual(script.suma(3, 2), 5)
+        self.assertEqual(script.suma(-1, 1), 0)
+        self.assertEqual(script.suma(-1, -1), -2)
 
 if __name__ == '__main__':
     unittest.main()
