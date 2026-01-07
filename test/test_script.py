342bd7ddfbfd38765e39a305b1d10f1986cfed1b
gcazcarroceste <gcazcarro@ceste.com>
2026-01-07 10:59:08 +0100

    Update test_script.py

--- test/test_script.py
+++ test/test_script.py
@@ -1,7 +1,1 @@
-def obtener_saludo():
-    """Retorna un saludo estandar"""
-    return "Hello world"
 
-if __name__ == "__main__":
-    # Esto solo se ejecuta si lanzas el script directamente, no al importarlo
-    print(obtener_saludo())
